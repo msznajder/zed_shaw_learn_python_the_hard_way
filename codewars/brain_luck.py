e5d58f004e131420a9238d9d67acdc786d8040c4
Michal Sznajder <msznajder@gmail.com>
2017-06-12 16:11:48 +0200

    Codewars - finished 'Brain luck' challenge.

--- codewars/brain_luck.py
+++ codewars/brain_luck.py
@@ -1,100 +1,84 @@
+from collections import defaultdict
+
 """
 https://www.codewars.com/kata/526156943dfe7ce06200063e
 https://fatiherikli.github.io/brainfuck-visualizer/#LD4sPFs+Wy0+Kz4rPDxdPj5bLTw8Kz4+XTw8PC1dPj4u
 """
 
+
 def brain_luck(code, input):
 
 	input_bytes_array = [ord(ch) for ch in input]
-	print(input_bytes_array)
 	
+	inpt_pointer_idx = 0
 	inst_pointer_idx = 0
-	data_pointer_idx = 0 # not sure hot to initialise
-	data_pointer_val = 0 # not sure hot to initialise
+	data_pointer_idx = 0
+	data_pointer_val = defaultdict(int)
 	res_array = [] 
 
 	counter = 0
 
 	while True:
 		instruction = code[inst_pointer_idx]
-		# print("inst_pointer_idx", inst_pointer_idx)
 		if instruction == '>':
-			print(">")
 			data_pointer_idx += 1
-			if data_pointer_idx > len(input_bytes_array)-1:
-				data_pointer_idx = len(input_bytes_array)-1
 			inst_pointer_idx += 1
 		elif instruction == '<':
-			print("<")
 			data_pointer_idx -= 1
-			if data_pointer_idx < 0:
-				data_pointer_idx = 0
 			inst_pointer_idx += 1
 		elif instruction == '+':
-			print("+")
-			data_pointer_val = (data_pointer_val + 1) % 256
+			data_pointer_val[data_pointer_idx] = (data_pointer_val[data_pointer_idx] + 1) % 256
 			inst_pointer_idx += 1
 		elif instruction == '-':
-			print("-")
-			data_pointer_val = (data_pointer_val - 1) % 256
+			data_pointer_val[data_pointer_idx] = (data_pointer_val[data_pointer_idx] - 1) % 256
 			inst_pointer_idx += 1
 		elif instruction == '.':
-			print(".")
-			res_array.append(chr(data_pointer_val))
-			data_pointer_val = 0 # not sure if I should do that here
+			res_array.append(chr(data_pointer_val[data_pointer_idx]))
 			inst_pointer_idx += 1
 		elif instruction == ',':
-			print(",")
-			data_pointer_val = input_bytes_array[data_pointer_idx] # this should be converted to ascii - how?
+			data_pointer_val[data_pointer_idx] = input_bytes_array[inpt_pointer_idx]
 			inst_pointer_idx += 1
-			data_pointer_idx += 1
+			inpt_pointer_idx += 1
 		elif  instruction == '[':
-			print("[")
-			if data_pointer_val == 0:
-				inst_pointer_idx = inst_pointer_idx + code[inst_pointer_idx:].find(']') + 1
+			if data_pointer_val[data_pointer_idx] == 0:
+				inst_pointer_idx = inst_pointer_idx + find_corresponding_closing_bracket_idx(code[inst_pointer_idx+1:]) + 1
 			else:
 				inst_pointer_idx += 1
-			print(inst_pointer_idx)
 		elif instruction == ']':
-			print("]")
-			if data_pointer_val != 0:
-				print(code[:inst_pointer_idx])
-				inst_pointer_idx = code[:inst_pointer_idx].rfind('[') + 1
+			if data_pointer_val[data_pointer_idx] != 0:
+				inst_pointer_idx = find_corresponding_opening_bracket_idx(code[:inst_pointer_idx]) + 1
 			else:
 				inst_pointer_idx += 1
-			print(inst_pointer_idx)
-
-		
-		# print("data_pointer_idx", data_pointer_idx)
-		# print("data_pointer_val", data_pointer_val)
-		# print("res_array", res_array)
 
 		if inst_pointer_idx >= len(code):
-			break
-
-		print(inst_pointer_idx)
-		counter += 1
-		if counter >= 345:
 			break
 
 	return "".join(res_array)
 
 
-# print(brain_luck(',+[-.,+]', 'Codewars' + chr(255)))
+def find_corresponding_closing_bracket_idx(s):
+	did_find_other_opening_counter = 0
+	for idx, ch in enumerate(s):
+		if ch == '[':
+			did_find_other_opening_counter += 1 
+		elif ch == ']' and did_find_other_opening_counter != 0:
+			did_find_other_opening_counter -= 1
+		elif ch == ']' and did_find_other_opening_counter == 0:
+			return idx
 
-# print(brain_luck(',[.[-],]', 'Codewars' + chr(0)))
 
+def find_corresponding_opening_bracket_idx(s):
+
+	did_find_other_closing_counter = 0
+	for idx, ch in enumerate(s[::-1]):
+		if ch == ']':
+			did_find_other_closing_counter += 1
+		elif ch == '[' and did_find_other_closing_counter != 0:
+			did_find_other_closing_counter -= 1
+		elif ch == '[' and did_find_other_closing_counter == 0:
+			return (len(s) - 1) - idx
+
+
+print(brain_luck(',+[-.,+]', 'Codewars' + chr(255)))
+print(brain_luck(',[.[-],]', 'Codewars' + chr(0)))
 print(brain_luck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', chr(8) + chr(9)))
-
-
-# print(brain_luck(',+[-.,+]', 'Codewars' + chr(255)))
-# brain_luck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', chr(8) + chr(9)), chr(72)
-
-# Echo until byte(255) encountered
-# assert brain_luck(',+[-.,+]', 'Codewars' + chr(255)), 'Codewars'
-
-# # Echo until byte(0) encountered
-# assert brain_luck(',[.[-],]', 'Codewars' + chr(0)), 'Codewars'
-
-# # Two numbers multiplier
-# assert brain_luck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', chr(8) + chr(9)), chr(72)
---
+++ codewars/find_corresponding_bracket.py
@@ -0,0 +1,44 @@
+def find_corresponding_opening_bracket_idx(s):
+
+	did_find_other_closing_counter = 0
+	for idx, ch in enumerate(s[::-1]):
+		if ch == ']':
+			did_find_other_closing_counter += 1
+		elif ch == '[' and did_find_other_closing_counter != 0:
+			did_find_other_closing_counter -= 1
+		elif ch == '[' and did_find_other_closing_counter == 0:
+			return (len(s) - 1) - idx
+
+
+# print(find_corresponding_opening_bracket_idx(',[.[-],')) # 1
+
+# print(find_corresponding_opening_bracket_idx(',[.[-')) # 3
+
+# print(find_corresponding_opening_bracket_idx(',>,<[>[->+>+<<]>>[-<<+>>')) # 17
+
+# print(find_corresponding_opening_bracket_idx(',>,<[>[->+>+<<]>>[-<<+>>]')) # 4
+
+# print(find_corresponding_opening_bracket_idx('[,>,<[>[->+>+<<]>>[-<<+>>]]')) # 0
+
+
+
+def find_corresponding_closing_bracket_idx(s):
+	did_find_other_opening_counter = 0
+	for idx, ch in enumerate(s):
+		if ch == '[':
+			did_find_other_opening_counter += 1 
+		elif ch == ']' and did_find_other_opening_counter != 0:
+			did_find_other_opening_counter -= 1
+		elif ch == ']' and did_find_other_opening_counter == 0:
+			return idx
+
+
+# print(find_corresponding_closing_bracket_idx('-.,+]')) # 4
+ 
+# print(find_corresponding_closing_bracket_idx('.[-],]')) # 5
+
+# print(find_corresponding_closing_bracket_idx('>[->+>+<<]>>[-<<+>>]]')) # 20
+
+# print(find_corresponding_closing_bracket_idx('->+>+<<]>>[-<<+>>]')) # 7
+
+# print(find_corresponding_closing_bracket_idx(',>,<[>[->+>+<<]>>[-<<+>>]]]')) # 26
