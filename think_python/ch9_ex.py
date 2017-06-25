650becd79b8491c39ff6541d69c78b138e2b7e9d
Michal Sznajder <msznajder@gmail.com>
2017-06-25 23:30:56 +0200

    Think Python - cleanup.

--- think_python/ch9_ex.py
+++ think_python/ch9_ex.py
@@ -154,16 +154,6 @@ print(is_abecedarian("deeeefa"))
 print_abecedarian_words()
 
 
-"""
-Exercise 7  
-This question is based on a Puzzler that was broadcast on the radio program Car Talk (http://www.cartalk.com/content/puzzlers):
-Give me a word with three consecutive double letters. I’ll give you a couple of words that almost qualify, but don’t. For example, the word committee, c-o-m-m-i-t-t-e-e. It would be great except for the ‘i’ that sneaks in there. Or Mississippi: M-i-s-s-i-s-s-i-p-p-i. If you could take out those i’s it would work. But there is a word that has three consecutive pairs of letters and to the best of my knowledge this may be the only word. Of course there are probably 500 more but I can only think of one. What is the word?
-Write a program to find it. Solution: http://thinkpython2.com/code/cartalk1.py.
-"""
-
-
-
-
 
 
 
