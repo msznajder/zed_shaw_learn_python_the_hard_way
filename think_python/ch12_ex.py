c0d9a9483d1db28077f8eb51d9d4b63dac6a2e50
Michal Sznajder <msznajder@gmail.com>
2017-07-07 17:41:17 +0200

    Think Python - Chapter 12 exercises.

--- think_python/ch12_ex.py
+++ think_python/ch12_ex.py
@@ -5,7 +5,7 @@ Write a function called most_frequent that takes a string and prints the letters
 def most_frequent(s):
 	hist = letters_hist(s)
 	hist_list = [(key, val) for key, val in hist.items()]
-	hist_list.sort(key=lambda x: x[1], reverse=True)
+	hist_list.sort(key=lambda x: x[1], reverse=True) # !!
 	print(list(zip(*hist_list))[0])
 
 
@@ -34,7 +34,6 @@ In Scrabble a “bingo” is when you play all seven tiles in your rack, along w
 Solution: http://thinkpython2.com/code/anagram_sets.py.
 """
 import ch10_ex
-import ch11_ex
 from collections import defaultdict
 
 def find_anagram_lists():
@@ -43,10 +42,46 @@ def find_anagram_lists():
 	for word in words:
 		key = tuple(sorted(list(word)))
 		anagrams_sets[key].append(word)
-	return sorted([val for key, val in anagrams_sets.items() if len(val) > 1], key=lambda val: len(val), reverse=True)
+	return sorted([val for key, val in anagrams_sets.items() if len(val) > 1], key=lambda val: len(val), reverse=True) # !!
 
-for val in find_anagram_lists():
-	print(val)
+# for val in find_anagram_lists():
+# 	print(val)
+
+"""
+Exercise 3  
+Two words form a “metathesis pair” if you can transform one into the other by swapping two letters; for example, “converse” and “conserve”. Write a program that finds all of the metathesis pairs in the dictionary. Hint: don’t test all pairs of words, and don’t test all possible swaps. Solution: http://thinkpython2.com/code/metathesis.py. Credit: This exercise is inspired by an example at http://puzzlers.org.
+"""
+import ch11_ex
+
+def find_metathesis_pairs():
+	words = ch10_ex.load_words()
+	words_dict = ch11_ex.dict_from_list(words)
+	metathesis_pairs = []
+	for word in words:
+		swaps = generate_all_characters_swaps(word)
+		for swap in swaps:
+			if swap in words_dict:
+				metathesis_pairs.append((word, swap))
+	return metathesis_pairs
+
+
+def generate_all_characters_swaps(s):
+	swaps = []
+	for i in range(len(s)):
+		for j in range(len(s)):
+			if i != j:
+				s_list = list(s)
+				s_list[i], s_list[j] = s_list[j], s_list[i] # !!
+				swap = "".join(s_list)
+				if s != swap:
+					swaps.append(swap)
+	return list(set(swaps))
+
+
+# print(generate_all_characters_swaps("0123"))
+# print(find_metathesis_pairs())
+
+## TODO: Use find_anagram_lists() to search for all and time both solutions.
 
 
 
@@ -54,3 +89,13 @@ for val in find_anagram_lists():
 
 
 
+
+
+
+
+
+
+
+
+
+	
